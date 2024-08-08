0b0f3a2cb421036ee92183001e02374dc0683bfe
Yathartha Aarush <145242949+Yathartha19@users.noreply.github.com>
2024-08-08 22:02:00 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,16 @@
 import { useEffect } from 'react';
 import './main.css';
+import Fade from 'react-reveal/Fade';
+
 
 const Hero = () => {
 
-  // useEffect(() => {
-  //   const contentElement = document.getElementById('hero');
-  //   if (contentElement) {
-  //     contentElement.scrollIntoView({ behavior: 'smooth' });
-  //   }
-  // } , []);
+  useEffect(() => {
+    const contentElement = document.getElementById('hero');
+    if (contentElement) {
+      contentElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  } , []);
 
   const scrollToAbout = () => {
 
@@ -23,42 +25,51 @@ const Hero = () => {
     <>
 
     <div id="hero" className="hero min-h-screen">
-      <div className="hero-overlay flex justify-center items-center h-screen" style={{ backgroundImage: "url('./images/hero.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+      <div className="hero-overlay flex justify-center items-center h-screen" style={{ backgroundImage: "url('./images/hero.png')", backgroundSize: 'cover', backgroundPosition: 'center' }} ></div>
       <div className="text-center wht pt-12">
-        <h1 className=" mb-6 text-4xl md:hidden font-bold text-">E - Cell</h1>
-        <h1 className=" mb-6 hidden md:flex text-6xl lg:text-7xl xl:text-8xl font-bold wht opening">Entrepreneurship Cell</h1>
-        <h4 className=" mb-16 text-3xl lg:text-4xl xl:text-5xl wht opening">PES University</h4>
-        <button className="openingnav border rounded-md py-2 px-4 hover:bg-white hover:text-black" onClick={scrollToAbout}>Explore</button>
+        <h1 className=" mb-6 text-6xl md:hidden font-bold font-cursive">E - Cell</h1>
+        <h1 className=" mb-6 hidden md:flex text-8xl lg:text-8xl xl:text-9xl font-bold wht opening font-cursive">Entrepreneurship Cell</h1>
+        <h4 className=" mb-16 text-3xl lg:text-3xl xl:text-4xl wht openingnav font-thin">PES University</h4>
+        <button className="openingnav border rounded-md py-2 px-4 hover:bg-white duration-200 hover:text-black" onClick={scrollToAbout}>Explore</button>
       </div>
     </div>
+
 
     <div id="about">
       <img src='./images/content.png' className='absolute -z-10 w-full' />
       <div className="flex flex-col items-center wht">
+        <Fade>
           <h1 className="text-2xl lg:text-3xl xl:text-4xl font-bold pt-40">About Us</h1>
           <div className="flex flex-row items-cente">
             <p className="blck flex mt-10 md:mt-16 ml-24 md:ml-40 mr-24 md:mr-40 text-xl lg:text-2xl xl:text-3xl font-thin text-center">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec diam eu lectus venenatis vulputate. Sed tincidunt mi nec ligula bibendum, ac fringilla mauris sagittis. Cras euismod nisi a purus vehicula, id egestas justo dictum. Phasellus quis tortor ac turpis facilisis vehicula et ut lectus. Integer vehicula sapien nec ante fermentum, non facilisis erat cursus. Aliquam erat volutpat. Mauris at facilisis ligula, sed varius justo.
             </p>
           </div>
+        </Fade>
         </div>
     </div>
+
     <br/>
 
     <div id="content" className="flex flex-col justify-center w-full bg-gradient-to-b from-black  to-black">
 
+
+        <Fade>
         <div className="flex flex-col mt-28 items-center wht">
           <h1 className="text-2xl lg:text-3xl xl:text-4xl font-bold">Our Goals</h1>
           <p className="blck flex mt-10 md:mt-16 ml-24 md:ml-40 mr-24 md:mr-40 text-xl lg:text-2xl xl:text-3xl font-thin text-center">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec diam eu lectus venenatis vulputate. Sed tincidunt mi nec ligula bibendum, ac fringilla mauris sagittis. Cras euismod nisi a purus vehicula, id egestas justo dictum. Phasellus quis tortor ac turpis facilisis vehicula et ut lectus. Integer vehicula sapien nec ante fermentum, non facilisis erat cursus. Aliquam erat volutpat. Mauris at facilisis ligula, sed varius justo.
           </p>
-        </div>
-        
+        </div>   
+        </Fade>
+   
         
         <div className='flex flex-col justify-center items-center wht mt-32 mb-20'>
           <h1 className="text-2xl lg:text-3xl xl:text-4xl font-bold">Events and Initiatives</h1>
 
           <div className='flex flex-col justify-center items-center mt-20 gap-16'>
+
+
 
             <div className='flex flex-col md:flex-row justify-center items-center gap-12'>
               <img src='./images/esummit.png' className='low:h-20 md:h-32 lg:h-64' />
@@ -84,11 +95,13 @@ const Hero = () => {
                 <p className='text-center text-lg lg:text-xl xl:text-2xl font-thin'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/> Nullam nec diam eu lectus venenatis vulputate.</p>
               </div>
             </div>
+
+
           
           </div>
 
         </div>
-      
+        
     </div>
 
     </>
